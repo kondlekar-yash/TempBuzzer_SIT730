0daa85a8105fc04032705fdb511906b1df566903
Yash Kondlekar <83708020+kondlekar-yash@users.noreply.github.com>
2022-06-06 23:29:11 +1000

    Update tempBuzz.py
    Refined code

--- tempBuzz.py
+++ tempBuzz.py
@@ -1,14 +1,21 @@
-import sys
-import Adafruit_DHT
+mport Adafruit_DHT as AdaDHT
 import RPi.GPIO as GPIO
 
+DHTSensor = AdaDHT.DHT11
+DHTPin = 4
 BuzzerPin = 3
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BuzzerPin, GPIO.OUT, initial=GPIO.LOW)
 
 while True:
-  humidity, temperature = Adafruit_DHT.read_retry(11,4)
-  print("Temp={0:0.1f}*C Humidity={1:0.1f}%".format(temperature, humidity))
-  if(temperature>17): GPIO.output(3, GPIO.HIGH)
-  else: GPIO.output(3, GPIO.LOW)
+	humidity, temperature = AdaDHT.read_retry(DHTSensor, DHTPin)
+	if humidity is not None and temperature is not None:
+		print("Temperature={0:0.1f}*C Humidity={1:0.1f}%".format(temperature,humidity))
+		if(temperature>32):
+			GPIO.output(BuzzerPin, GPIO.HIGH)
+		else:
+			GPIO.output(BuzzerPin, GPIO.LOW)
+	else:
+        	print("Failed to retrieve data from sensor")
